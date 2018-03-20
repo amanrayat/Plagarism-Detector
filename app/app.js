f07b899490e82e83614d46092a55c35ceb485e87
Aman Rayat <rayat.a@husky.neu.edu>
2018-03-20 00:27:52 -0400

    adding project

---
+++ app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Form from './components/Form';
+import Navbar from './components/Navbar';
+import CourseTable from './components/CourseTable';
+
+ReactDOM.render(
+  <div>
+    <Navbar />
+    {/*<Form />*/}
+    <CourseTable />
+  </div>
+  , root);
